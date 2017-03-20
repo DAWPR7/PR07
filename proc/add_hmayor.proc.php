b5f354b56c5f4a8a1feae4e867916afc3869c8b6
mlopezgalan <lopezgalan.miguel@gmail.com>
2017-03-20 19:45:26 +0100

    COmprobaciones en las inserciones

--- proc/add_hmayor.proc.php
+++ proc/add_hmayor.proc.php
@@ -1,16 +1,47 @@
 <?php 
 	extract($_GET);
 	require_once("conexion.proc.php");
-	//SQL de inserción
-	$sql = "INSERT INTO `tbl_user` (`user_matricula`, `user_pwd`) VALUES ('".$hm_mat."', '".$pass."');";
-	//Insertamos el usuario en la BD
-		mysqli_query($conexion,$sql);
-	//cojemos el user_id de la ultima inserción realizada
-		$last_id = mysqli_insert_id($conexion);
-
-	//Insertamos las notas con el último usuario insertado 
-		$sql_insert2 = "INSERT INTO `tbl_hmayor` (`user_id`, `hma_notas`) VALUES ('".$last_id."', '".$notes."');";
-		mysqli_query($conexion,$sql_insert2);
-	echo "<script type='text/javascript'>alert('El hermano mayor ".$hm_mat." ha sido dado de alta con éxito');
-			location.href='../add_hmayor.php';</script>";
+	//Creamos una consulta para comprobar si existe un Hermano Mayor con esa matricula
+		$comprobacion = "SELECT * FROM `tbl_user` WHERE `user_matricula` = ".$hm_mat."; ";
+				//Hacemos la petición a la BD
+				$result	=	mysqli_query($conexion,$comprobacion);
+				//Contamos los resultados que nos devuelve la consulta anterior
+				$total  = mysqli_num_rows($result); 
+	//Si el SQL nos encuentra un resultado significa que ya existe
+	if($total>=1)
+	{
+		echo "<script type='text/javascript'>alert('¡ERROR! el hermano mayor ".$hm_mat." ya existe');
+				location.href='../add_hmayor.php';</script>";
+	}
+	else //Si no dvuelve nada, significa que no existe y se puede insertar
+	{
+		//SQL de inserción
+		$sql = "INSERT INTO `tbl_user` (`user_matricula`, `user_pwd`) VALUES ('".$hm_mat."', '".$pass."');";
+		//Insertamos el usuario en la BD
+			$resultado = mysqli_query($conexion,$sql);
+			if($resultado)
+				{
+				//cojemos el user_id de la ultima inserción realizada
+					$last_id = mysqli_insert_id($conexion);
+					//Insertamos las notas con el último usuario insertado 
+					$sql_insert2 = "INSERT INTO `tbl_hmayor` (`user_id`, `hma_notas`) VALUES ('".$last_id."', '".$notes."');";
+					$resultado2 = mysqli_query($conexion,$sql_insert2);
+					if($resultado2)
+					{
+						echo "<script type='text/javascript'>alert('El hermano mayor ".$hm_mat." ha sido dado de alta con éxito');
+						location.href='../add_hmayor.php';</script>";
+					}
+					else
+					{
+						echo "<script type='text/javascript'>alert('¡Ups! Error inesperado, porfavor, vuelva a intentarlo');
+						location.href='../add_hmayor.php';</script>";
+					}//End else error insert2
+				}
+			else
+			{
+				echo "<script type='text/javascript'>alert('¡Ups! Error inesperado, porfavor, vuelva a intentarlo');
+				location.href='../add_hmayor.php';</script>";
+			}//End else error insert
+		
+	}//End Else existente
 ?>
