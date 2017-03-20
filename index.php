374b541d3df411b6126f6688c8fc20ca108e39ae
Sergi <sergitnsr@gmail.com>
2017-03-20 17:33:33 +0100

    Cambios organización carpetas

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
 	<title>Hermano Mayor</title>
 	<meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="estilos.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="css/login.css" />
 	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
 </head>
 <body>
