0b97e5009e17b87a734a0829737184a0b4baae8d
Sergi <sergitnsr@gmail.com>
2017-03-21 17:40:17 +0100

    Cambios Sergi

--- SERGI/index.php
+++ SERGI/index.php
@@ -1,60 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html>
+<head>
+	<!-- META -->
+	<meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- LINKS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/login.css" />
+	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+	<!-- TITLE -->
+	<title>Hermano Mayor</title>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Aplicación para detectar posibles casos de bullying y juntar alumnos mayores con alumnos menores.">
-    <meta name="author" content="Dani, Sergi, Musta y Miguel">
-    <link rel="icon" href="assets/img/favicon.png">
+</head>
 
-    <title>Hermano Mayor</title>
+<body background="assets/img/login.jpg">
+	<!-- DIV LOGIN -->
+	<div class="container">
+        <div class="card card-container">
+						<!-- LOGO -->
+            <img id="profile-img" class="profile-img-card" src="assets/img/logo.png" />
+            <p id="profile-name" class="profile-name-card"></p>
+						<!-- FORMULARIO LOGIN -->
+						<form action="login.proc.php" method="POST" class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Matrícula" required autofocus>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Iniciar sesión</button>
+            </form>
 
-    <!-- CSS -->
-    <link href="assets/css/main.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Hermano Mayor</a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Inicio</a></li>
-            <li><a href="#about">Informa de un problema</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-    <div class="container">
-
-      <div class="base">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery.min.js"><\/script>')</script>
-  </body>
+  </div>
+<!-- INCUDE FOOTER -->
+<?php
+include("footer.php");
+?>
+<!-- SCRIPTS -->
+<script src="assets/js/jquery-3.2.0.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/login.js"></script>
+</body>
 </html>
