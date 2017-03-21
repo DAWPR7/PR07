0906bb6c5d5fbfa6bb4d54c5399f5f17822c5730
Musta <8664.joan23@fje.edu>
2017-03-21 22:00:25 +0100

    denuncia anonima y registro menor

---
+++ registro_menor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hermano Mayor</title>
+	<meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/login.css" />
+	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
+</head>
+<body background="IMG/login.jpg">
+
+
+<!-- Footer -->
+<?php
+include("footer.php");
+?>
+<script src="assets/js/jquery-3.2.0.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/login.js"></script>
+</body>
+</html>
