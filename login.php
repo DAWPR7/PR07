584b9dfe999fac64f305af88736eb67fa4292fd9
Musta <8664.joan23@fje.edu>
2017-03-21 15:54:18 +0100

    login, estilos, libro estilos
    El libro de estilos le he dado prioridad porque he visto que se entrega
    mañana. Sergi ponte con el logo y con el por favor ;)

--- login.php
+++ login.php
@@ -9,26 +9,25 @@
 	<link rel="stylesheet" href="assets/css/login.css" />
 	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
 </head>
-<body>
-  <h1 class="pen-title">Hermano Mayor</h1>
+<body background="IMG/login.jpg">
 
-
-	<div class="module form-module">
-  	<div class="toggle"> </div>
-    
-	
-  <div class="form">
-    <h2>Accede a tu cuenta</h2>
-    
-    <form action="" method="POST">
-      <input id="usuario" name="usuario" type="email" placeholder="Email"/>
-      <input id="password" name="password" type="password" placeholder="Contraseña"/>
-      <button>Acceder</button>
-    </form>
-  </div>
-	</div>
-	
-
+<div class="container">
+        <div class="card card-container">
+            <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
+            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" class="profile-name-card"></p>
+            <form action="login.proc.php" method="POST" class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+               
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Iniciar sesión</button>
+            </form><!-- /form -->
+            <!--<a href="#" class="forgot-password">
+                Forgot the password?
+            </a> -->
+        </div><!-- /card-container -->
+    </div><!-- /container -->
 
 
 <!-- Footer -->
@@ -37,5 +36,6 @@ include("footer.php");
 ?>
 <script src="assets/js/jquery-3.2.0.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/login.js"></script>
 </body>
 </html>
