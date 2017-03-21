e25884e67b95bc3996c11f4917da23ab2ca4e9af
Sergi <sergitnsr@gmail.com>
2017-03-21 18:56:48 +0100

    CHAT HERMANO Mayor/menor
    Falta revisar variables y jquery/ajax

---
+++ SERGI/chat-menor/index.php
@@ -0,0 +1,111 @@
+<?
+session_start();
+
+if(isset($_GET['logout'])){
+
+	//Simple exit message
+	$fp = fopen("log.html", 'a');
+	fwrite($fp, "<div class='msgln'><i>". $_SESSION['name'] ." se ha desconectado.</i><br></div>");
+	fclose($fp);
+
+	session_destroy();
+	header("Location: index.php"); //Redirect the user
+}
+
+function loginForm(){
+	echo'
+	<div id="loginform">
+	<form action="index.php" method="post">
+		<p>Introduce tu usuario para continuar:</p>
+		<label for="name">Usuario:</label>
+		<input type="text" name="name" id="name" />
+		<input type="submit" name="enter" id="enter" value="Enviar" />
+	</form>
+	</div>
+	';
+}
+
+if(isset($_POST['enter'])){
+	if($_POST['name'] != ""){
+		$_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+	}
+	else{
+		echo '<span class="error">Escribe tu usuario</span>';
+	}
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Chat - Customer Module</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+
+<?php
+if(!isset($_SESSION['name'])){
+	loginForm();
+}
+else{
+?>
+<div id="wrapper">
+	<div id="menu">
+		<p class="welcome">Hola, <b><?php echo $_SESSION['name']; ?></b></p>
+		<p class="logout"><a id="exit" href="#">Desconectarse</a></p>
+		<div style="clear:both"></div>
+	</div>
+	<div id="chatbox"><?php
+	if(file_exists("log.html") && filesize("log.html") > 0){
+		$handle = fopen("log.html", "r");
+		$contents = fread($handle, filesize("log.html"));
+		fclose($handle);
+
+		echo $contents;
+	}
+	?></div>
+
+	<form name="message" action="">
+		<input name="usermsg" type="text" id="usermsg" size="63" />
+		<input name="submitmsg" type="submit"  id="submitmsg" value="Enviar" />
+	</form>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+	//If user submits the form
+	$("#submitmsg").click(function(){
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});
+		$("#usermsg").attr("value", "");
+		return false;
+	});
+
+	//Load the file containing the chat log
+	function loadLog(){
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}
+		  	},
+		});
+	}
+	setInterval (loadLog, 2500);	//Reload file every 2.5 seconds
+
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Seguro que quieres desconectarte?");
+		if(exit==true){window.location = 'index.php?logout=true';}
+	});
+});
+</script>
+<?php
+}
+?>
+</body>
+</html>
