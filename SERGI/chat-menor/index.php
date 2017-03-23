23cf7702c2d53dd86ee810154304f7345a2dc58c
Sergi <sergitnsr@gmail.com>
2017-03-23 16:32:10 +0100

    Cambio index chat menor

--- SERGI/chat-menor/index.php
+++ SERGI/chat-menor/index.php
@@ -37,7 +37,7 @@ if(isset($_POST['enter'])){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Chat - Customer Module</title>
+<title>Chat</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 
