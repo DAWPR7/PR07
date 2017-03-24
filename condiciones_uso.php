34dd3c333c49971d4e514bc51bfd34186c527f8c
Musta <8664.joan23@fje.edu>
2017-03-24 15:20:03 +0100

    regitro menor

---
+++ condiciones_uso.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hermano Mayor</title>
+	<meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/login.css" />
+	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
+</head>
+<body background="IMG/login.jpg">
+<div class="container">
+<div class="row">
+   <div class=""><a href='index.php'><i class='fa fa-arrow-circle-left fa-3x' aria-hidden='true' title='Atrás'></i></a></br></div>
+</div>
+<div class="card">
+  <div class="card-block">
+    <h4 class="card-title">Condiciones Generales de Uso</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Descripción</h6>
+    <p class="card-text">Hermano Mayor ha desarrollado una plataforma de ayuda contra el acoso escolar a la que se puede acceder desde el sitio web www.hermanomayor.es, diseñada para poner en contacto a los alumnos que sufren acoso con otros alumnos que puedan servir de ayuda, de forma que puedan compartir vivencias y ayudarse.
+
+Los presentes términos y condiciones tienen por objeto regular el acceso y los términos de uso de la Plataforma. Se ruega leer atentamente estas condiciones. Entiendes y reconoces que Hermano Mayor no es parte en ningún contrato, acuerdo o relación contractual, de ninguna clase, suscrito entre los Usuarios de su Plataforma.
+
+Al hacer clic en “Registrarse con un usuario”, reconoces que has leído y aceptado las presentes condiciones generales de uso.</p>
+
+
+
+<!-- Footer -->
+<?php
+include("footer.php");
+?>
+<script src="assets/js/jquery-3.2.0.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/login.js"></script>
+</body>
+</html>
--- registro_menor.php
+++ registro_menor.php
@@ -10,8 +10,28 @@
 	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
 </head>
 <body background="IMG/login.jpg">
-
-
+<div class="container">
+<div class="row">
+   <div class=""><a href='index.php'><i class='fa fa-arrow-circle-left fa-3x' aria-hidden='true' title='Atrás'></i></a></br></div>
+</div>
+<div class="card card-container">
+                   
+            <p id="profile-name" class="profile-name-card"></p>
+            <form action="registro_menor.proc.php" method="POST" class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+                <input type="password" name="password1" id="inputPassword" class="form-control" placeholder="Password" required>
+               	<input type="password" name="password2" id="inputPassword" class="form-control" placeholder=" Confirma Password" required>
+                <button class="btn-primary btn btn-danger" type="submit">Confirmar</button>
+            <p class="form-group">Al continuar confirmas que has leído y que estás de acuerdo con las <a href="condiciones_uso.php">Condiciones de uso</a> y <a href="politica_privacidad.php">Política de privacidad</a>.</p>
+						<hr>
+						<p></p>¿Ya tienes cuenta? <a href="index.php">Inicia sesión</a></p>
+            </form><!-- /form -->
+            <!--<a href="#" class="forgot-password">
+                Forgot the password?
+            </a> -->
+        </div><!-- /card-container -->
+    </div><!-- /container -->
 <!-- Footer -->
 <?php
 include("footer.php");
