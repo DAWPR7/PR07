62402af287024cf9fd3905a2913171f9ed7f8781
Sergi <sergitnsr@gmail.com>
2017-03-27 16:13:07 +0200

    Cambios img y menormayor

--- menormayor.php
+++ menormayor.php
@@ -7,17 +7,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/login.css" />
-	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
+	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 </head>
 <body background="IMG/login.jpg">
 <div class="container">
 <div class="row">
    <div class=""><a href='index.php'><i class='fa fa-arrow-circle-left fa-3x' aria-hidden='true' title='Atrás'></i></a></br></div>
 </div>
-<div class="">
-		<span class="image"><a href="#"><img src="IMG/menor.jpg" alt="" class="img-rounded" title="Hermano Menor" /></span></a>
-		<span class="image"><a href="#"><img src="IMG/mayor.jpg" alt=""  class="img-rounded" title="Hermano Mayor" /></span></a>
+<div class="row">
+	<div class="col-md-6 col-md-offset-4">
+			<a href="hmenor.php"><img src="IMG/menor.png" alt="" class="img-rounded" title="Hermano Menor" /></a>
+	</div>
+	<div class="col-md-6 col-md-offset-4">
+			<a href="hmenor.php"><img src="IMG/mayor.png" alt="" class="img-rounded" title="Hermano Menor" /></a>
+	</div>
 </div>
+
+
 </div>
 
 <!-- Footer -->
