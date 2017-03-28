98009d9c7d24b12e5cb2a9f8fe849604f5166cab
mlopezgalan <lopezgalan.miguel@gmail.com>
2017-03-28 15:58:23 +0200

    Cambio sentencia SQL en sh_terapias

--- sh_terapias.php
+++ sh_terapias.php
@@ -11,6 +11,7 @@
 				SELECT *
 					FROM `tbl_historial` RIGHT JOIN `tbl_hmenor`
 						ON `tbl_historial`.`hme_id` = `tbl_hmenor`.`hme_id`
+                        RIGHT JOIN `tbl_user` ON `tbl_hmenor`.`user_id` = `tbl_user`.`user_id`
 					WHERE `tbl_hmenor`.`hme_estado` = 'ocupado'";
 			$sh_terapias = mysqli_query($conexion, $sh_terapias_sql);
 			/*while($terapia = mysqli_fetch_array($sh_terapias))
