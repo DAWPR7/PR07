03ca51bdb86e67966fefd65655fb212570eb3206
Musta <8664.joan23@fje.edu>
2017-03-28 15:58:34 +0200

    validación nano

--- DANI/pr07_def/registro_menor.php
+++ DANI/pr07_def/registro_menor.php
@@ -8,6 +8,20 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/login.css" />
 	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>	
+<!--EL SCRIPT ESTE VALIDO LOS 2 CAMPOS SEAN IGUALES -->
+<script> 
+function comprobarClave(){ 
+    password1 = document.f1.password1.value 
+    password2 = document.f1.password2.value 
+
+    if (password1 == password2) 
+        return true; 
+    else 
+        alert("Las contraseñas deben de ser identicas")
+        return false;
+} 
+</script> 
+<!--FIN DEL SCRIPT PEÑITA -->
 </head>
 <body background="IMG/login.jpg">
 <div class="container">
@@ -17,12 +31,12 @@
 <div class="card card-container">
                    
             <p id="profile-name" class="profile-name-card"></p>
-            <form action="registro_menor.proc.php" method="POST" class="form-signin">
+            <form action="registro_menor.proc.php" method="POST" class="form-signin" name="f1">
                 <span id="reauth-email" class="reauth-email"></span>
                 <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
                 <input type="password" name="password1" id="inputPassword" class="form-control" placeholder="Contraseña" required>
                	<input type="password" name="password2" id="inputPassword" class="form-control" placeholder=" Confirmar contraseña" required>
-                <button class="btn-primary btn btn-danger" type="submit">Confirmar</button>
+                <button class="btn-primary btn btn-danger" type="submit" onClick="return comprobarClave()">Confirmar</button>
             <p class="form-group">Al continuar confirmas que has leído y que estás de acuerdo con las <a href="condiciones_uso.php">Condiciones de uso</a> y <a href="politica_privacidad.php">Política de privacidad</a>.</p>
 						<hr>
 						<p>¿Ya tienes cuenta? <a href="index.php">Inicia sesión</a></p>
