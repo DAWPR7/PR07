bffb5478e05fb7b0e011a9bfe909f6598cd39015
danvabe <danvabe@hotmail.com>
2017-03-28 15:26:31 +0200

    Actualizado
    Añadido cerrar_sesion

--- DANI/pr07_def/administrador.php
+++ DANI/pr07_def/administrador.php
@@ -40,7 +40,7 @@ if (!isset($_SESSION['admin'])) {
 <div class="row">
    <div class=""><a href='index.php'><i class='fa fa-arrow-circle-left fa-3x' aria-hidden='true' title='Atrás'></i></a></br></div>
 </div>
-<div class="card card-container">
+<div class="container" style="background-color: white;">
        
     <h1>Administración</h1><br><h3>Psicólogos</h3>
 <table class="table table-striped">
