f85b79785ea2fd20c6f098eb3033f2cdcf20ce37
Sergi <sergitnsr@gmail.com>
2017-03-30 16:38:23 +0200

    Include Titulo ORGANIZADA

---
+++ PR_07_HM/PR07_HM_ORGANIZADA/includes/titulo.php
@@ -0,0 +1,3 @@
+<div class="page-header">
+  <h1>Hermano Mayor  <span class="pull-right"><a href='index.php'><i class='fa fa-arrow-circle-left' aria-hidden='true' title='Atrás'></i></a>&nbsp;&nbsp;<a href="cerrar_sesion.php"><i class="fa fa-sign-out" aria-hidden="true" title='Desconectarse'></i></a></span></h1><br/>
+</div>
